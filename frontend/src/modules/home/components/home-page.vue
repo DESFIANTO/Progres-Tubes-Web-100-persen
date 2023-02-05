55523ef42a2d8f205b6ba3970d3241925e5c0c6e
yann <fiandesfianto@gmail.com>
2023-02-06 00:27:09 +0700

    first commit

---
+++ frontend/src/modules/home/components/home-page.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p class="home-bottom-message">
+      
+      <img src="https://cdn.discordapp.com/attachments/1063807822959235082/1071805385851281428/img.png.png"/> 
+    </p>
+  </div>
+</template>
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'app-home-page',
+
+
+  created() {
+    if (this.isMobile) {
+      this.collapseMenu();
+    }
+  },
+  computed: {
+    ...mapGetters({
+      isMobile: 'layout/isMobile',
+    })
+  },
+
+  methods: {
+    ...mapActions({
+      collapseMenu: 'layout/collapseMenu',
+    })
+  }
+};
+</script>
+
+<style scoped>
+.el-col {
+  margin-bottom: 16px;
+}
+
+.home-bottom-message {
+  width: 100%;
+  text-align: center;
+  color: grey;
+}
+@import '../../../../public/input.css';
+</style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/modules/home/components/home-page.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
